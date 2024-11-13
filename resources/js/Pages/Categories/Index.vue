024611831ead50f3f37fa74c23a7bb0e072f3aa0
vimuths123 <vimuths@yahoo.com>
2024-11-13 09:42:46 +0530

    Add admin layout

--- resources/js/Pages/Categories/Index.vue
+++ resources/js/Pages/Categories/Index.vue
@@ -1,35 +1,42 @@
 <template>
-  <div>
-    <h1>Categories</h1>
-    <Link href="/categories/create" class="btn btn-primary">Add New Category</Link>
+  <AdminLayout>
+    <div>
+      <h1>Categories</h1>
+      <Link href="/categories/create" class="btn btn-primary"
+        >Add New Category</Link
+      >
 
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Description</th>
-          <th>Parent Category</th> <!-- Add a column for Parent Category -->
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="category in categories" :key="category.id">
-          <td>{{ category.name }}</td>
-          <td>{{ category.description }}</td>
-          <td>{{ category.parent ? category.parent.name : 'None' }}</td> <!-- Display parent category name if it exists -->
-          <td>
-            <Link :href="`/categories/${category.id}/edit`">Edit</Link>
-            <button @click="deleteCategory(category.id)">Delete</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Parent Category</th>
+            <!-- Add a column for Parent Category -->
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="category in categories" :key="category.id">
+            <td>{{ category.name }}</td>
+            <td>{{ category.description }}</td>
+            <td>{{ category.parent ? category.parent.name : "None" }}</td>
+            <!-- Display parent category name if it exists -->
+            <td>
+              <Link :href="`/categories/${category.id}/edit`">Edit</Link>
+              <button @click="deleteCategory(category.id)">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </AdminLayout>
 </template>
 
 <script setup>
-import { Link, useForm } from '@inertiajs/vue3';
-import { defineProps } from 'vue';
+import { Link, useForm } from "@inertiajs/vue3";
+import { defineProps } from "vue";
+import AdminLayout from "@/Layouts/AdminLayout.vue";
 
 const props = defineProps({
   categories: Array,
@@ -38,7 +45,7 @@ const props = defineProps({
 const form = useForm({});
 
 const deleteCategory = (id) => {
-  if (confirm('Are you sure you want to delete this category?')) {
+  if (confirm("Are you sure you want to delete this category?")) {
     form.delete(`/categories/${id}`);
   }
 };
