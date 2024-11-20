8a87b76879e68027823073c42e7738305c51cc43
vimuths123 <vimuths@yahoo.com>
2024-11-20 13:05:18 +0530

    Trait for unique code

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory, GeneratesUniqueCode ;
+    use HasFactory, GeneratesUniqueCode;
     protected $fillable = [
         'category_id',
         'name',
