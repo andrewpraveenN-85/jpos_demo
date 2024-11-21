63a4b34c0962d934f3ed5b46c116f6d29340f812
vimuths123 <vimuths@yahoo.com>
2024-11-21 15:20:55 +0530

    Fix count issue

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -42,7 +42,7 @@ class ProductController extends Controller
 
         $count = $productsQuery->count();    
 
-        $products = $productsQuery->paginate(2);
+        $products = $productsQuery->paginate(8);
 
         $allcategories = Category::with('parent')->get();
         $colors = Color::all();
