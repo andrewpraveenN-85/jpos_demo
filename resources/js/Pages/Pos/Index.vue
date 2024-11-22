0f672d0a2a684dc486e5b817c94d53ac4c3371b8
Nadishan Chathuranga <nadishanchathuranga21@gmail.com>
2024-11-22 15:49:45 +0530

    pos page style

--- resources/js/Pages/Pos/Index.vue
+++ resources/js/Pages/Pos/Index.vue
@@ -6,39 +6,54 @@
     <!-- Include the Header -->
     <Header />
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <div class="w-5/6 py-12 space-y-16">
-      <div class="flex items-center justify-between">
-        <div class="w-full flex space-x-4">
+      <div class="flex items-center justify-between space-x-4">
+        <div class="flex w-full space-x-4">
           <a
             href="./includes/dash_page.php"
-            class="text-3xl rounded-full border-2 border-black p-1 text-black"
+            class="p-1 text-3xl text-black border-2 border-black rounded-full"
           >
             <p><i class="ri-arrow-left-line"></i></p>
           </a>
-          <p class="text-4xl font-bold uppercase tracking-wide text-black">
+          <p class="text-4xl font-bold tracking-wide text-black uppercase">
             PoS
           </p>
         </div>
-        <div class="w-full flex justify-between items-center space-x-4">
+        <div class="flex items-center justify-between w-full space-x-4">
           <p class="text-3xl font-bold tracking-wide text-black">
             Order ID : 202234344
           </p>
           <p class="text-3xl text-black"><i class="ri-restart-line"></i></p>
         </div>
       </div>
-      <div class="w-full flex gap-4">
-        <div class="w-1/3 flex flex-col">
-          <div class="w-full flex flex-col">
-            <div class="p-16 bg-black shadow-lg space-y-8 rounded-3xl">
-              <p class="text-5xl text-white font-bold mb-4">Customer Details</p>
+      <div class="flex w-full gap-4">
+        <div class="flex flex-col w-1/2">
+          <div class="flex flex-col w-full">
+            <div class="p-16 space-y-8 bg-black shadow-lg rounded-3xl">
+              <p class="mb-4 text-5xl font-bold text-white">Customer Details</p>
               <div class="mb-3">
                 <input
                   type="text"
                   placeholder="Enter Customer Name"
-                  class="w-full px-4 py-4 text-black placeholder-black rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  class="w-full px-4 py-4 text-black placeholder-black bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
               </div>
-              <div class="flex mb-3 gap-2 text-black">
+              <div class="flex gap-2 mb-3 text-black">
                 <select
                   class="w-[60px] px-2 py-2 bg-white placeholder-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 >
@@ -49,72 +64,72 @@
                 <input
                   type="text"
                   placeholder="Enter Customer Contact Number"
-                  class="flex-grow px-4 py-4 bg-white text-black placeholder-black rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  class="flex-grow px-4 py-4 text-black placeholder-black bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
               </div>
               <div class="text-black">
                 <input
                   type="email"
                   placeholder="Enter Customer Email"
-                  class="w-full px-4 py-4 bg-white text-black placeholder-black rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  class="w-full px-4 py-4 text-black placeholder-black bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
               </div>
             </div>
           </div>
           <div
-            class="w-full justify-center items-center flex flex-col pt-32 space-y-8"
+            class="flex flex-col items-center justify-center w-full pt-32 space-y-8"
           >
-            <!-- <div class="w-full flex flex-col items-center space-y-4">
+            <!-- <div class="flex flex-col items-center w-full space-y-4">
               <input
                 type="text"
                 v-model="form.barcode"
                 placeholder="Enter Barcode"
-                class="w-2/3 px-4 py-3 text-black placeholder-black rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+                class="w-2/3 px-4 py-3 text-black placeholder-black bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
               <button
                 @click="submitBarcode"
-                class="px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700 focus:outline-none"
+                class="px-6 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-700 focus:outline-none"
               >
                 Scan Barcode
               </button>
             </div> -->
             <!-- <img
               src="/images/Fading wheel.gif"
-              class="h-32 w-32 object-cover rounded-full"
+              class="object-cover w-32 h-32 rounded-full"
             />
             <p class="text-3xl text-black">
               Bar Code Scanner is in Progress...
             </p> -->
           </div>
         </div>
-        <div class="w-2/3 flex flex-col border-4 border-black p-8 rounded-3xl">
-          <div class="w-full flex flex-col justify-center items-start px-12">
-            <h2 class="text-5xl text-black font-bold">Billing Details</h2>
+        <div class="flex flex-col w-1/2 p-8 border-4 border-black rounded-3xl">
+          <div class="flex flex-col items-start justify-center w-full px-12">
+            <h2 class="text-5xl font-bold text-black">Billing Details</h2>
             <p v-if="products.length === 0" class="text-2xl text-red">
               No Products to show
             </p>
             <div
-              class="w-full flex py-4 items-center border-b border-black"
+              class="flex items-center w-full py-4 border-b border-black"
               v-for="item in products"
               :key="item.id"
             >
               <div class="flex w-1/6">
                 <img
                   :src="item.image"
-                  class="h-16 w-16 object-cover rounded-lg"
+                  class="object-cover w-16 h-16 rounded-lg"
                 />
               </div>
               <div
-                class="flex flex-col justify-start items-start space-y-4 w-4/6"
+                class="flex flex-col items-start justify-start w-4/6 space-y-4"
               >
                 <p class="text-3xl text-black">
                   {{ item.name }}
                 </p>
-                <div class="flex w-full justify-between items-center">
-                  <div class="w-full flex justify-start items-center space-x-4">
+                <div class="flex items-center justify-between w-full">
+                  <div class="flex items-center justify-start w-full space-x-4">
                     <p
                       @click="incrementQuantity(item.id)"
-                      class="bg-black h-8 w-8 text-white flex justify-center items-center rounded cursor-pointer"
+                      class="flex items-center justify-center w-8 h-8 text-white bg-black rounded cursor-pointer"
                     >
                       <i class="ri-add-line"></i>
                     </p>
@@ -125,17 +140,17 @@
                     </p>
                     <p
                       @click="decrementQuantity(item.id)"
-                      class="bg-black h-8 w-8 text-white flex justify-center items-center rounded cursor-pointer"
+                      class="flex items-center justify-center w-8 h-8 text-white bg-black rounded cursor-pointer"
                     >
                       <i class="ri-subtract-line"></i>
                     </p>
                   </div>
-                  <div class="flex justify-center items-center">
+                  <div class="flex items-center justify-center">
                     <p class="text-black">{{ item.selling_price }} LKR</p>
                   </div>
                 </div>
               </div>
-              <div class="flex w-1/6 justify-end">
+              <div class="flex justify-end w-1/6">
                 <p
                   @click="removeProduct(item.id)"
                   class="text-3xl text-black border-2 border-black rounded-full cursor-pointer"
@@ -144,27 +159,27 @@
                 </p>
               </div>
             </div>
-            <div class="w-full space-y-2 pt-6">
-              <div class="w-full flex justify-between items-center px-16">
+            <div class="w-full pt-6 space-y-2">
+              <div class="flex items-center justify-between w-full px-16">
                 <p class="text-xl">Sub Total</p>
                 <p class="text-xl">{{ subtotal }} LKR</p>
               </div>
               <div
-                class="w-full flex justify-between items-center py-2 px-16 border-b border-black pb-4"
+                class="flex items-center justify-between w-full px-16 py-2 pb-4 border-b border-black"
               >
                 <!-- <p class="text-xl">Discount</p>
                 <p class="text-xl">1500.00 LKR</p> -->
               </div>
-              <div class="w-full flex justify-between items-center px-16 pt-4">
+              <div class="flex items-center justify-between w-full px-16 pt-4">
                 <p class="text-3xl text-black">Total</p>
                 <p class="text-3xl text-black">{{ total }} LKR</p>
               </div>
             </div>
-            <div class="w-full flex flex-col space-y-8">
+            <div class="flex flex-col w-full space-y-8">
               <div
-                class="w-full flex justify-center items-center space-x-8 pt-8"
+                class="flex items-center justify-center w-full pt-8 space-x-8"
               >
-                <p class="text-black text-xl">Payment Method :</p>
+                <p class="text-xl text-black">Payment Method :</p>
                 <div
                   class="w-[80px] h-[50px] border border-black rounded-xl flex flex-col justify-center items-center text-center"
                 >
@@ -178,11 +193,11 @@
                   <p>Credit Card</p>
                 </div>
               </div>
-              <div class="w-full flex justify-center items-center">
+              <div class="flex items-center justify-center w-full">
                 <p
-                  class="cursor-pointer bg-black text-white font-bold uppercase tracking-wider w-full text-center py-4 rounded-xl text-2xl"
+                  class="w-full py-4 text-2xl font-bold tracking-wider text-center text-white uppercase bg-black cursor-pointer rounded-xl"
                 >
-                  <i class="ri-add-circle-fill pr-4"></i> Confirm Order
+                  <i class="pr-4 ri-add-circle-fill"></i> Confirm Order
                 </p>
               </div>
             </div>
