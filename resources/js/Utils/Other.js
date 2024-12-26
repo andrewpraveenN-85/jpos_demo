173e88c215b48327761893f6e06ad3987ae2afdd
vimuths123 <vimuths@yahoo.com>
2024-12-27 01:01:14 +0530

    Change

---
+++ resources/js/Utils/Other.js
@@ -0,0 +1,7 @@
+export const generateOrderId = () => {
+    const characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    return Array.from({ length: 6 }, () =>
+        characters.charAt(Math.floor(Math.random() * characters.length))
+    ).join("");
+};
