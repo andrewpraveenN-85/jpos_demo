de17e7f60a266abaa184246912828bbedf4f1d38
vimuths123 <vimuths@yahoo.com>
2024-12-26 10:25:21 +0530

    Fix stock transition

--- resources/js/Pages/StockTransaction/Index.vue
+++ resources/js/Pages/StockTransaction/Index.vue
@@ -130,7 +130,7 @@
                   {{ stock.transaction_date || "N/A" }}
                 </td>
                 <td class="p-4 border-gray-200">
-                  {{ stock.product?.supplier?.name || "N/A" }}
+                  {{ stock?.product?.supplier?.name || "N/A" }}
                 </td>
               </tr>
             </tbody>
