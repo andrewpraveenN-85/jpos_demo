0d33e6bebde1e115658e591c310adf8085bd1c5b
vimuths123 <vimuths@yahoo.com>
2025-01-03 17:49:44 +0530

    New changes

--- resources/js/Pages/Products/Promotions.vue
+++ resources/js/Pages/Products/Promotions.vue
@@ -1,3 +1,46 @@
 <template>
-  asa
+  <Head title="Sizes" />
+  <Banner />
+  <div
+    class="flex flex-col items-center justify-start min-h-screen py-8 space-y-8 bg-gray-100 px-36"
+  >
+    <!-- Include the Header -->
+    <Header />
+    <div class="w-5/6 py-12 space-y-24">
+      <div class="flex items-center justify-between">
+        <div class="flex items-center justify-center space-x-4"></div>
+        
+      </div>
+      <div class="flex w-full">
+        <div class="flex items-center w-full h-16 space-x-4 rounded-2xl">
+          <Link href="/products">
+            <img src="/images/back-arrow.png" class="w-14 h-14" />
+          </Link>
+          <p class="text-4xl font-bold tracking-wide text-black uppercase">
+            Products / Add Promotions
+          </p>
+        </div>
+        <div class="flex justify-end w-full">
+          
+        </div>
+      </div>
+
+       <div class="overflow-x-auto">sasa</div>
+     
+    </div>
+  </div>
+
+  <Footer />
 </template>
+
+<script setup>
+import { ref } from "vue";
+import { useForm } from "@inertiajs/vue3";
+import { Link } from "@inertiajs/vue3";
+import { Head } from '@inertiajs/vue3';
+import Header from "@/Components/custom/Header.vue";
+import Footer from "@/Components/custom/Footer.vue";
+import Banner from "@/Components/Banner.vue";
+import { HasRole } from "@/Utils/Permissions";
+</script>
+
